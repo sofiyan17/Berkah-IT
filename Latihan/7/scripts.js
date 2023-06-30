5ab954be4476d75fe0dcfb0c4c7106b877fc9608
sofiyan17 <muhammadsofian535@gmail.com>
2023-06-30 16:33:32 +0800

    Post

---
+++ Latihan/7/scripts.js
@@ -0,0 +1,13 @@
+var bulan = [1, 2, 3, 4, 5]
+bulan[1]
+
+var angka = [1, 2, 3, 4, 5[6, 7, 8, 9, 10]]
+// angka[5][3]
+// console.log(angka[5][3])
+
+var person = {
+    firstname:'muh',
+    lastname:"Sofiyan",
+    age:21
+}
+person['age']
